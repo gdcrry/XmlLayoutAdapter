684915fc871214d5e979cc609a5a589b0e5a19bd
zhangtianye <zhangtianye.bugfree@bytedance.com>
2019-01-29 19:36:06 +0800

    upgrade gradle version

--- app/src/main/java/com/example/gdcrry/layoutadapterdemo/layout_adapter/XmlLayoutAdapterImpl.java
+++ app/src/main/java/com/example/gdcrry/layoutadapterdemo/layout_adapter/XmlLayoutAdapterImpl.java
@@ -8,7 +8,6 @@ import android.transition.TransitionManager;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
